6a9e162b4dba7ae05c44a4df84e3710ffea4a279
Ahinsak Raj <117904029+ahinsakraj@users.noreply.github.com>
2023-12-21 16:01:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if(num == 0 || num == 1) {
+		return 0;
+	}
+	let x = 0;
+	let y = 1;
+	for(let i = 2; i < num; i++) {
+		let temp = x;
+		x = y;
+		y = x + y;
+	}
+	return y;
 }
 
 module.exports = fibonacci;
